8732d0e6388548bc5a5eb7c69e7c7c494f5cddf0
stephen shen <723809534@qq.com>
2020-10-29 17:02:59 +0800

    完成demo provider

---
+++ spring-cloud-demo2020-provider/src/main/java/cn/hellomyheart/spring/cloud/demo2020/controller/HelloController.java
@@ -0,0 +1,19 @@
+package cn.hellomyheart.spring.cloud.demo2020.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description
+ * @className: HelloController
+ * @package: cn.hellomyheart.spring.cloud.demo2020.controller
+ * @author: Stephen Shen
+ * @date: 2020/10/29 下午4:34
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/provider/hello/hi")
+    public String hi(){
+        return "OK-"+System.currentTimeMillis();
+    }
+}
