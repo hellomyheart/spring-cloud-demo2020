2bd5336f6d9f6667b1c4f33983aa89764993bf4a
stephen shen <723809534@qq.com>
2020-10-29 18:03:07 +0800

    完成 demo 生产者扩展

--- spring-cloud-demo2020-provider/src/main/java/cn/hellomyheart/spring/cloud/demo2020/provider/controller/HelloController.java
+++ spring-cloud-demo2020-provider/src/main/java/cn/hellomyheart/spring/cloud/demo2020/provider/controller/HelloController.java
@@ -1,7 +1,11 @@
 package cn.hellomyheart.spring.cloud.demo2020.provider.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @description
@@ -15,5 +19,19 @@ public class HelloController {
     @GetMapping("/provider/hello/hi")
     public String hi(){
         return "OK-"+System.currentTimeMillis();
+    }
+
+    @GetMapping("/provider/hello/hi2")
+    public String hi2(@RequestParam String num){
+        return "ok"+ num;
+    }
+
+    @GetMapping("/provider/hello/hi3")
+    public List hi3(@RequestParam int count){
+        List<String> list =new ArrayList<>();
+        for(int i=1;i<=count;i++){
+            list.add("demo"+i);
+        }
+        return list;
     }
 }
